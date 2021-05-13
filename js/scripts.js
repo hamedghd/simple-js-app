2870aaff74060a1f18e3609ceb6ee44478819305
hamedghd <h.gharibdoost@gmail.com>
2021-05-14 01:52:47 +0200

    Update the push method..

--- js/scripts.js
+++ js/scripts.js
@@ -73,32 +73,33 @@ const pokemonRepository = (function () {
   };
 })();
 
-pokemonList.push({ name: 'Charmeleon', height: 1.1, weight: 19.0, types: ['fire'] });
-pokemonList.push({ name: 'Charizard', height: 1.7, weight: 90.5, types: ['fire', 'flying'] });
-pokemonList.push({ name: 'Squirtle', height: 0.5, weight: 9.0, types: ['water'] });
-pokemonList.push({ name: 'Wartortle', height: 1.0, weight: 22.5, types: ['water'] });
-pokemonList.push({ name: 'Blastoise', height: 1.6, weight: 85.5, types: ['water'] });
-pokemonList.push({ name: 'Caterpie', height: 0.3, weight: 2.9, types: ['bug'] });
-pokemonList.push({ name: 'Metapod', height: 0.7, weight: 9.9, types: ['bug'] });
-pokemonList.push({ name: 'Butterfree', height: 1.1, weight: 32, types: ['bug', 'flying'] });
-pokemonList.push({ name: 'Weedle', height: 0.3, weight: 3.2, types: ['bug', 'poison'] });
-pokemonList.push({ name: 'Kakuna', height: 0.6, weight: 10.0, types: ['bug', 'poison'] });
-pokemonList.push({ name: 'Beedrill', height: 1.0, weight: 29.5, types: ['bug', 'poison'] });
-pokemonList.push({ name: 'Pidgey', height: 0.3, weight: 1.8, types: ['flying', 'normal'] });
-pokemonList.push({ name: 'Pidgeotto', height: 1.1, weight: 30, types: ['flying', 'normal'] });
-pokemonList.push({ name: 'Pidgeot', height: 1.5, weight: 39.5, types: ['flying', 'normal'] });
-pokemonList.push({ name: 'Rattata', height: 0.3, weight: 3.5, types: ['normal'] });
-pokemonList.push({ name: 'Raticate', height: 0.7, weight: 18.5, types: ['normal'] });
-pokemonList.push({ name: 'Spearow', height: 0.3, weight: 2.0, types: ['flying', 'normal'] });
-pokemonList.push({ name: 'Fearow', height: 1.2, weight: 38.0, types: ['flying', 'normal'] });
-pokemonList.push({ name: 'Ekans', height: 2.0, weight: 6.9, types: ['poison'] });
-pokemonList.push({ name: 'Arbok', height: 3.5, weight: 65.0, types: ['poison'] });
-pokemonList.push({ name: 'Pikachu', height: 0.4, weight: 6.0, types: ['electric'] });
-pokemonList.push({ name: 'Raichu', height: 0.8, weight: 30.0, types: ['electric'] });
-pokemonList.push({ name: 'Sandshrew', height: 0.6, weight: 12.0, types: ['ground'] });
-pokemonList.push({ name: 'Sandslash', height: 1.0, weight: 29.5, types: ['ground'] });
-pokemonList.push({ name: 'Nidoran', height: 0.4, weight: 7.0, types: ['poison'] });
-pokemonList.push({ name: 'Nidorina', height: 0.8, weight: 20.0, types: ['poison'] });
+// Adds new database.
+pokemonRepository.add({ name: 'Charmeleon', height: 1.1, weight: 19.0, types: ['fire'] });
+pokemonRepository.add({ name: 'Charizard', height: 1.7, weight: 90.5, types: ['fire', 'flying'] });
+pokemonRepository.add({ name: 'Squirtle', height: 0.5, weight: 9.0, types: ['water'] });
+pokemonRepository.add({ name: 'Wartortle', height: 1.0, weight: 22.5, types: ['water'] });
+pokemonRepository.add({ name: 'Blastoise', height: 1.6, weight: 85.5, types: ['water'] });
+pokemonRepository.add({ name: 'Caterpie', height: 0.3, weight: 2.9, types: ['bug'] });
+pokemonRepository.add({ name: 'Metapod', height: 0.7, weight: 9.9, types: ['bug'] });
+pokemonRepository.add({ name: 'Butterfree', height: 1.1, weight: 32, types: ['bug', 'flying'] });
+pokemonRepository.add({ name: 'Weedle', height: 0.3, weight: 3.2, types: ['bug', 'poison'] });
+pokemonRepository.add({ name: 'Kakuna', height: 0.6, weight: 10.0, types: ['bug', 'poison'] });
+pokemonRepository.add({ name: 'Beedrill', height: 1.0, weight: 29.5, types: ['bug', 'poison'] });
+pokemonRepository.add({ name: 'Pidgey', height: 0.3, weight: 1.8, types: ['flying', 'normal'] });
+pokemonRepository.add({ name: 'Pidgeotto', height: 1.1, weight: 30, types: ['flying', 'normal'] });
+pokemonRepository.add({ name: 'Pidgeot', height: 1.5, weight: 39.5, types: ['flying', 'normal'] });
+pokemonRepository.add({ name: 'Rattata', height: 0.3, weight: 3.5, types: ['normal'] });
+pokemonRepository.add({ name: 'Raticate', height: 0.7, weight: 18.5, types: ['normal'] });
+pokemonRepository.add({ name: 'Spearow', height: 0.3, weight: 2.0, types: ['flying', 'normal'] });
+pokemonRepository.add({ name: 'Fearow', height: 1.2, weight: 38.0, types: ['flying', 'normal'] });
+pokemonRepository.add({ name: 'Ekans', height: 2.0, weight: 6.9, types: ['poison'] });
+pokemonRepository.add({ name: 'Arbok', height: 3.5, weight: 65.0, types: ['poison'] });
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, weight: 6.0, types: ['electric'] });
+pokemonRepository.add({ name: 'Raichu', height: 0.8, weight: 30.0, types: ['electric'] });
+pokemonRepository.add({ name: 'Sandshrew', height: 0.6, weight: 12.0, types: ['ground'] });
+pokemonRepository.add({ name: 'Sandslash', height: 1.0, weight: 29.5, types: ['ground'] });
+pokemonRepository.add({ name: 'Nidoran', height: 0.4, weight: 7.0, types: ['poison'] });
+pokemonRepository.add({ name: 'Nidorina', height: 0.8, weight: 20.0, types: ['poison'] });
 
 pokemonList.forEach(function (item, index, array) {
   // opens an unordered list tags
